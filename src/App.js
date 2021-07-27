b99915b99ac46afd782c043523183222a52312ba
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-07-27 20:53:50 -0300

    requisitos 12 e 13 e ajustes no AddMovie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
+import movies from './data';
 import './App.css';
 
 function App() {
@@ -8,6 +11,8 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,36 @@
-// implement MovieLibrary component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+// import AddMovie from './AddMovie';
+// import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+  //     movies: { movies },
+  //   };
+  // }
+
+  render() {
+    // const { movies } = this.props;
+    // const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.array.isRequired,
+// };
+
+export default MovieLibrary;
